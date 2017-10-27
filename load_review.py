0e94281ca069f94c0270ec6a486e6ee64822d44e
deepanshu <deepanshuwadhwa97@gmail.com>
2017-10-28 02:31:47 +0530

    Added reviews table to dB and created script for loading reviews

---
+++ load_review.py
@@ -0,0 +1,25 @@
+import sqlite3
+import json
+import sys
+a=''
+lst=[]
+s=1
+db=sqlite3.connect('details')
+cur=db.cursor()
+name=""
+cur.execute('SELECT id, cid, rid, uid, expert, text from reviews where cid = ?',(str(sys.argv[1]),))
+ab=cur.fetchall()
+print (ab)
+r=0
+s=0
+D={}
+for i in ab:
+   
+	for t in i:
+		r+=1
+		a+=str(t)
+		a+='#'
+	D[s]=a
+	s+=1
+print (json.dumps(D))
+sys.exit()
