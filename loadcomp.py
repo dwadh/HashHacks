d4f3f623b0bff72ce007ed02f9cedb778f9c6e44
deepanshu <deepanshuwadhwa97@gmail.com>
2017-10-28 05:44:29 +0530

    Added script for loading company details

---
+++ loadcomp.py
@@ -0,0 +1,26 @@
+import sqlite3
+import json
+import sys
+a=''
+lst=[]
+s=1
+db=sqlite3.connect('details')
+cur=db.cursor()
+name=""
+cur.execute('SELECT id, name, desc, req, left, trans from company where id = ?',(str(sys.argv[1]),))
+ab=cur.fetchall()
+
+r=0
+for i in ab:
+   
+	for t in i:
+		r+=1
+		if r==6 and t == 'None':
+			continue
+		a+=str(t)
+		a+='#'
+
+D={}
+D[0]=a
+print (json.dumps(D))
+sys.exit()
