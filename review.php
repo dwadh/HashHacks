547ead7ef30a36894b98552a53b926e2bb081d69
deepanshu <deepanshuwadhwa97@gmail.com>
2017-10-28 10:26:18 +0530

    Reviews fixed

--- review.php
+++ review.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html>
-<style>
-#myProgress {
-  width: 100%;
-  background-color: #ddd;
-}
-
-#myBar {
-  width: 1%;
-  height: 30px;
-  background-color: #4CAF50;
-}
-</style>
-<body onload="move()">
 <?php
 $val = $_GET['comp'];
 $result=json_decode(exec('python load_review.py '.$val),true);
@@ -28,19 +13,9 @@ foreach($result as &$value)
     }
 
     echo "<br>$text<br><br>By: $by<br><hr><br><br>";
+
     
 }
-
-<div id="myProgress">
-  <div id="myBar"></div>
-</div>
-<script>
-function move() {
-	  var elem = document.getElementById("myBar");   
-	  var width = .3;
-	  elem.style.width = ((width+1)/2)*100 + '%';
-	}
-</script>
-
-</body>
-</html>
+$result= exec('python sentiment.py '. $val);
+echo "<br><br><br>The Trust level is : " . $result;
+?>
