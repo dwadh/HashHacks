0f31dfceca78ee5e93cd7f4862deca047724e46f
deepanshu <deepanshuwadhwa97@gmail.com>
2017-10-28 05:33:58 +0530

    Transaction Process script

--- process_trans.py
+++ process_trans.py
@@ -12,20 +12,31 @@ cur=db.cursor()
 company = int(sys.argv[1])
 investor = int(sys.argv[2])
 amount = int(sys.argv[3])
-cur.execute('SELECT req, left FROM company WHERE id = ?', (company,))
+cur.execute('SELECT req, left, trans FROM company WHERE id = ?', (company,))
 ammt = cur.fetchone()
 req = float(ammt[0])
 left = float(ammt[1])
+c_trans = str(ammt[2])
 share = int(amount)/req
 new_amt = left - amount
-cur.execute('SELECT balance FROM investor WHERE id = ?', (investor,))
-balance = int(cur.fetchone()[0])
+cur.execute('SELECT balance, trans FROM investor WHERE id = ?', (investor,))
+u_dat = cur.fetchone()
+balance = int(u_dat[0])
+u_trans = str(u_dat[1])
 new_balance = balance - amount
 cur.execute('''INSERT INTO transac(cid, uid, amt, share, datee) VALUES (?,?,?,?,?)''', (company, investor, amount, share, datee))
 cur.execute('''UPDATE company SET left = ? WHERE id = ?''', (new_amt, company,))
 cur.execute('''UPDATE investor SET balance = ? WHERE id = ?''', (new_balance, investor,))
 db.commit()
 
+cur.execute('SELECT id FROM transac WHERE cid = ? AND uid = ?', (company, investor,))
+obt_data = cur.fetchall()[-1]
+t_id = str(obt_data[0])
+u_trans = u_trans + ',' + t_id
+c_trans = c_trans + ',' + t_id
+cur.execute('''UPDATE company SET trans = ? WHERE id = ?''', (c_trans, company,))
+cur.execute('''UPDATE investor SET trans = ? WHERE id = ?''', (u_trans, investor,))
+db.commit()
 '''
 
 
